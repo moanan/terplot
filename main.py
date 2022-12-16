6db86c677f8c197ab18e5c18e7c0f23e750b9a0b
anmo <mo@is.mpg.de>
2022-12-16 19:04:44 +0800

    add license and readme

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
 ﻿"""
 A python script to plot multiple channels of time-series data from terminal output.
-Very useful for debugging setpoint vs system output in robotic applications, e.g. PID controllers.
 
 Usage:
 python plot.py -t <X axis index length> -c <data channel index (start from 0)>
 
-Run the following command for a demo live plotting:
-(the terminal output from syn.py will be plotted, for data channel 0, 1, 2)
-python3 syn.py | python3 main.py -t 500 -c 0,1,2
-
-You might want to replace the delimiter if you are not using 'Tab', search and update the following line:
-parts = data.split('\t') # note: last element is '\n'
-
-You can easily debug a remote device with ssh connection, e.g.:
-ssh your_remote@192.168.2.9 'sudo ./your_project/your_program' | python \Desktop/plot_script.py -t 100 -c 4,13
 
 An Mo
 2022-Dec-16
 moan1992@gmail.com
-
-Adapted from 
-https://manashpratim.com/plot-realtime-terminal-data
-Manash Pratim Das (mpdmanash@iitkgp.ac.in)
 """
 
 import sys, getopt
