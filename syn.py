23f1eca0e70999038bf28ad7350a92e16b5f77cc
anmo <mo@is.mpg.de>
2022-12-16 20:38:30 +0800

    rounding digits

--- syn.py
+++ syn.py
@@ -7,7 +7,8 @@ while True:
     x = math.sin(float(i)*3.14/180)
     y = math.cos(float(i)*3.14/180)
     z = -x
-    print(str(x)+"\t"+str(y)+"\t"+str(z))
+    # print(str(x)+"\t"+str(y)+"\t"+str(z))
+    print("{:10.2f}".format(x) + "\t" + "{:10.2f}".format(y) + "\t" + "{:10.2f}".format(z))
     sys.stdout.flush()
     i = i+1
     time.sleep(0.01)
